914468e6ed9fb73e7534c9a3b689aa492e5ddd87
张稀虹 <zz332191120@mail.tust.cn>
2016-01-10 23:03:58 +0800

    remove useless code

--- dist/canvas-player.js
+++ dist/canvas-player.js
@@ -1,16 +1,5 @@
 (function (cp) {
 	'use strict';
-	// var a = new cp.player({
-	// 	type: "test",
-	// 	resources: {
-	// 		src: "resources/images",
-	// 		count: 341,
-	// 		bit: 3,
-	// 		wildcard: "out_",
-	// 		fileType: "jpg",
-	// 		manifest:[]
-	// 	}
-	// })
 	cp.initPlayer = function (params) {
 		cp.player = this;
 		this.defaultOpts = {
@@ -19,8 +8,8 @@
 				height: 240,
 				scale: 1,
 				type: 'images', //images ,bigImg or video
-				selector: '#canvas-player',
-				audio: '#audio',
+				selector: 'canvas-player',
+				audio: 'audio',
 				poster: '',
 				loop: false,
 				buffer: 50, //define how many frames should be buffered
@@ -52,12 +41,12 @@
 			},
 			this.preload = {},
 			this._opts = merge(this.defaultOpts, params),
-			this.el = typeof this._opts.selector === 'string' ? document.querySelector(this._opts.selector) : this._opts.selector,
-			this.ela = typeof this._opts.audio === 'string' ? document.querySelector(this._opts.audio) : this._opts.audio;
+			this.el = typeof this._opts.selector === 'string' ? document.querySelector('#' + this._opts.selector) : this._opts.selector,
+			this.ela = typeof this._opts.audio === 'string' ? document.querySelector('#' + this._opts.audio) : this._opts.audio;
 		if (!this.el) {
 			return console.warn('Error. No element found for selector', this._opts.selector);
 		}
-		this.initCanvas();
+		this.init();
 		if (!this.ela) {
 			return console.warn('Error. No audio element found', this._opts.audio);
 		}
@@ -83,35 +72,47 @@
 		},
 		play: function (index) {
 			var p = cp.player;
-			if (index) {
-				console.log('call play at ' + this.status.last);
-			} else {
-				var ctx = p.canvas.getContext('2d');
-				p.timer = window.setInterval(function () {
-					if (p.status.current >= p._opts.resources.count) {
-						p.pause();
-						p.status.end();
-						return;
-					}
-					var _image = p.preload.getResult(p._opts.resources.manifest[p.status.current++].id);
-					var images = new createjs.Bitmap(_image);
-					ctx.drawImage(images.image, 0, 0, p.canvas.width, p.canvas.height);
-				}, 1000 / p._opts.fps);
-				// playAudio('#audio1');
-				p.status.played = true;
-				p.status.playing = true;
+			switch (p._opts.type) {
+			case 'images':
+				if (index) {
+					console.log('call play at ' + this.status.last);
+				} else {
+					var ctx = p.canvas.getContext('2d');
+					p.timer = window.setInterval(function () {
+						if (p.status.current >= p._opts.resources.count) {
+							p.pause();
+							p.status.end();
+							return;
+						}
+						var _image = p.preload.getResult(p._opts.resources.manifest[p.status.current++].id);
+						var images = new createjs.Bitmap(_image);
+						ctx.drawImage(images.image, 0, 0, p.canvas.width, p.canvas.height);
+					}, 1000 / p._opts.fps);
+					p.audio.play();
+					p.status.played = true;
+					p.status.playing = true;
 
+				}
+				break;
+			case 'bigImg':
+				break;
+			case 'video':
+				break;
+			default:
+				console.error('unsupport source type!');
+				return;
 			}
 		},
 		pause: function () {
 			this.status.last = this.status.current;
+			this.audio.pause();
 			window.clearInterval(this.timer);
 		},
 		buffering: function () {
 
 		},
-		initCanvas: function () {
-			this.canvas = $(this._opts.selector)[0];
+		init: function () {
+			this.canvas = document.getElementById(this._opts.selector);
 			this.canvas.width = this._opts.width;
 			this.canvas.height = this._opts.height;
 			if (!this.canvas.getContext) {
@@ -121,6 +122,7 @@
 			this.stage = new createjs.Stage(canvas);
 			this.stage.scaleX = this._opts.scale;
 			this.stage.scaleY = this._opts.scale;
+			this.audio = document.getElementById(this._opts.audio);
 		},
 		handleFileLoad: function () {
 
@@ -154,109 +156,6 @@
 
 	function PrefixInteger(num, length) {
 		return (Array(length).join('0') + num).slice(-length);
-	}
-
-	//Play when resources loaded 25%+
-	function handlePreplay() {
-		$('#loading').addClass('hidden');
-		timerA = window.setInterval(function () {
-			handleTick("manifest", timerA);
-		}, 1000 / properties.fps);
-		playAudio('#audio1');
-	}
-
-	//load complete
-	function handleComplete(evt) {
-		// $('#loading').addClass('hidden');
-		// console.log('load complete!');
-		// console.log(Date());
-		// timerA = window.setInterval(function () {
-		// 	handleTick("manifest");
-		// }, 1000 / properties.fps);
-	}
-
-	function loadError() {
-		console.log('load error');
-	}
-
-	function handleTick(target, timer) {
-		if (pauseCount !== -1 && counter !== -2) {
-			counter = pauseCount;
-			pauseCount = -1;
-		}
-		if (canvas.pause === true)
-			return;
-		_image = preload.getResult(properties[target][counter].id);
-		if (counter + 100 < properties[target].length - 1) {
-			nextImage = preload.getResult(properties[target][counter + 100].id);
-			if (nextImage === undefined) {
-				window.clearInterval(timer);
-				canvas.pause = true;
-				pauseCount = counter;
-				pauseFunc(target, timer);
-				return;
-			}
-		}
-		images = new createjs.Bitmap(_image);
-		ctx = canvas.getContext("2d");
-		ctx.drawImage(images.image, 0, 0, canvas.width, canvas.height);
-		counter++;
-		if (counter > properties[target].length - 1) {
-			stage.play = false;
-			counter = 0;
-			window.clearInterval(timerA);
-			window.clearInterval(timerB);
-			window.clearInterval(timerC);
-			window.clearInterval(timerPause);
-			if (target === "manifestC") {
-				document.removeEventListener('touchmove', touchMoveFunc, false);
-				$('#canvas-input').toggleClass('hidden');
-			}
-			if (target === "manifestE" || target === "manifestD") {
-				document.addEventListener('touchend', function () {
-					window.location.href = 'http://m.sjawards.com';
-				}, false);
-				counter = -2;
-			}
-			document.addEventListener('touchmove', touchMoveFunc, false);
-		}
-	}
-
-	function pauseFunc(target, timer) {
-		$("#progress").addClass("hidden");
-		$("#loading").toggleClass("hidden");
-		$("#progressC").toggleClass("hidden");
-		progressText = "已加载 " + (preload.progress * 100 | 0) + " %";
-		$('#progressC').text(progressText);
-		if (target == "manifestD") {
-			pauseAudio("#audio3");
-		}
-		if (target == "manifestE") {
-			pauseAudio("#audio2");
-		}
-		timerPause = window.setInterval(function () {
-			if (counter + 100 < properties[target].length - 1) {
-				cacheImage = preload.getResult(properties[target][counter + 100].id);
-				if (cacheImage) {
-					if (canvas.pause === false)
-						return;
-					$("#loading").toggleClass("hidden");
-					window.clearInterval(timerPause);
-					//console.log(timerPause);
-					if (target == "manifestD") {
-						playAudio('#audio3');
-					}
-					if (target == "manifestE") {
-						playAudio('#audio2');
-					}
-					timer = window.setInterval(function () {
-						handleTick(target, timer);
-					}, 1000 / properties.fps);
-					canvas.pause = false;
-				}
-
-			}
-		}, 2000);
 	}
 
 	function merge() {
--- sample/js/sample.js
+++ sample/js/sample.js
@@ -1,13 +1,15 @@
-var canvas, video, audio, timer, canvasPlayer;
+var canvas, video, audio, timer, canvasPlayer, canvasPlayer1;
 
 
 $(document).ready(function () {
 	canvas = document.getElementById('canvas3');
 	video = document.getElementById('video');
 	canvasPlayer = new cp.initPlayer({
+		id: 'canvasPlayer1',
 		width: 320,
 		height: 240,
 		type: "images",
+		selector: 'canvas1',
 		resources: {
 			src: "resources/images",
 			count: 341,
@@ -17,6 +19,21 @@ $(document).ready(function () {
 			manifest: []
 		}
 	});
+	// canvasPlayer1 = new cp.initPlayer({
+	// 	id: 'canvasPlayer2',
+	// 	selector: 'canvas2',
+	// 	width: 400,
+	// 	height: 300,
+	// 	type: "images",
+	// 	resources: {
+	// 		src: "resources/images",
+	// 		count: 341,
+	// 		bit: 3,
+	// 		wildcard: "out_",
+	// 		fileType: "jpg",
+	// 		manifest: []
+	// 	}
+	// });
 	$('#btn-video-play').click(function () {
 		video.play();
 	});
@@ -32,6 +49,13 @@ $(document).ready(function () {
 	$('#btn-images-pause').click(function () {
 		canvasPlayer.pause();
 	});
+	// $('#btn-big-play').click(function () {
+	// 	canvasPlayer1.load();
+	// 	canvasPlayer1.play();
+	// });
+	// $('#btn-big-pause').click(function () {
+	// 	canvasPlayer1.pause();
+	// });
 	//video source canvas
 	var v = document.getElementById("video");
 	var canvas3 = document.getElementById("canvas3");
